16eb911f1190df8c93158901b1b2ccf37ee64d3b
Maisie Wells <maisiejwells@gmail.com>
2019-03-06 17:34:05 -0700

    push

--- main.js
+++ main.js
@@ -2,16 +2,23 @@ import { films } from './starwars/films.js'
 
 const intro = document.querySelector('.starwars')
 
-films.forEach( (film) => {
-    let titleElement = document.createElement('h1')
-    intro.appendChild(titleElement)
-titleElement.textContent = film.title
+films.sort((a,b) => (a.episode_id > b.episode_id) ? 1 : -1 )
 
-let crawlElement = document.createElement('h3')
-intro.appendChild(crawlElement)
-crawlElement.textContent = film.opening_crawl
-let lineBreak =document.createElement('hr')
-intro.appendChild(lineBreak)
+films.forEach(film => {
+  let tile = document.createElement('div')
+tile.className = "tile"
+  let titleElement = document.createElement('h1')
+  tile.appendChild(titleElement)
+  titleElement.textContent = film.title
+
+
+  let crawlElement = document.createElement('div')
+  crawlElement.className = "crawl"
+  tile.appendChild(crawlElement)
+  crawlElement.textContent = film.opening_crawl
+
+  intro.appendChild(tile)
 })
+
 
 console.log(films)
